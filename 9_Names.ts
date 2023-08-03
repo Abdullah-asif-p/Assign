a7f968f8db5148c2d920986a34270a441deb8b8b
Abdullah <abdullahasifmf007+github@gmail.com>
2023-08-03 22:07:31 +0500

    First commit

---
+++ 9_Names.ts
@@ -0,0 +1,5 @@
+const Names = ["Hamza", "Ali", "Ahsan", "Hassan"];
+let len: number = Names.length;
+// loop to print all the names in the Array(Names) one by in one 
+for (let i: number = 0; i < len; i++) 
+    console.log(Names[i]);
