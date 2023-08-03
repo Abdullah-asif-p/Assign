413e29b24f9bfa5252d9bbbea3393eb977ea9818
Abdullah <abdullahasifmf007+github@gmail.com>
2023-08-03 21:50:40 +0500

    second commit

--- 5_Stripping-Names.ts
+++ 5_Stripping-Names.ts
@@ -1,6 +1,6 @@
 const Name = "\t\n   Shahid Mahmood   \t\n";
 console.log(Name);
-// Remove all space and newline character the string
+// Remove all space and newline character in string(name) and creates a new string
 const strippedName = Name.trim()
 console.log(strippedName)
 
