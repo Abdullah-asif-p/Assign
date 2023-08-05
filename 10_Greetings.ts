869d5c6d2b25e7997a7a10169f40757a4135db49
Abdullah <abdullahasifmf007+github@gmail.com>
2023-08-05 11:21:16 +0500

    First commit

---
+++ 10_Greetings.ts
@@ -0,0 +1,6 @@
+const Names = ["Hamza", "Ali", "Ahsan", "Hassan"];
+let len: number = Names.length;
+// loop to print all the names in the Array(Names) one by in one 
+for (let i: number = 0; i < len; i++) 
+    // Greet each person in array using template literals
+    console.log(`Hello,${Names[i]}`);
