bd441891ee58703abd7d9736fe3808743c1fe48d
Abdullah <abdullahasifmf007+github@gmail.com>
2023-08-05 11:21:20 +0500

    First commit

---
+++ 11_Your-Own-Array.ts
@@ -0,0 +1,9 @@
+const car = [
+  "Bugatti Type 57SC Atlantic",
+  "Lamborghini Veneno",
+  "Pagani Huayra",
+  "Koenigsegg Trevita",
+];
+let lenarray: number = car.length;
+for (let i = 0; i < lenarray; i++)
+    console.log(`I harbor a strong desire to acquire ${car[i]}`);
